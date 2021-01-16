4bcc8363d6b9703f60c999b092a6657356f66e07
Sergio Manzanares Gonzalez <manzanaresgonzalezsergio@gmail.com>
2021-01-16 18:39:05 +0100

    Inicial

---
+++ app/src/main/java/com/example/proyectoeprobador/PrendasActivity.java
@@ -0,0 +1,14 @@
+package com.example.proyectoeprobador;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PrendasActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_prendas);
+    }
+}
