97d4f74c65e82e32be48e0ef7cdc950d79579890
Sergio Manzanares Gonzalez <manzanaresgonzalezsergio@gmail.com>
2021-01-17 15:15:38 +0100

    Con el armario, feed y prendas

---
+++ app/src/main/java/com/example/proyectoeprobador/ArmarioActivity.java
@@ -0,0 +1,14 @@
+package com.example.proyectoeprobador;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ArmarioActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_armario);
+    }
+}
